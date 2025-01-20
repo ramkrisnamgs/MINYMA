8c7627b1f44e7301c46b5ba572b36638ed4def56
ramkrisnamgs <ramkrisnamgs@gmail.com>
2025-01-20 11:50:54 +0530

    Project completed

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,8 @@ import toast from 'react-hot-toast';
 import { io } from 'socket.io-client';
 
 
-const BASE_URL = "http://localhost:5001";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : 
+    "/";
 
 export const useAuthStore = create((set, get) => ({
     authUser: null,
@@ -99,11 +100,6 @@ export const useAuthStore = create((set, get) => ({
         userId: authUser._id,
       }
     });
-
-    socket.on("connect_error", (error) => {
-        console.error("Socket connection error:", error);
-    });
-
     socket.connect();
 
     set({ socket: socket });
